2a3b2059db716af645a976c27981d1669dbddb47
JIgor-Silva <joao.igor@thinkor.com.br>
2025-04-01 18:51:40 -0300

    implementação da denylist

---
+++ app/services/jwt_service.rb
@@ -0,0 +1,17 @@
+require 'jwt'
+
+class JwtService
+  SECRET_KEY = Rails.application.secrets.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now) #token com payload
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY, 'HS256')
+  end
+
+  def self.decode(token) #decodificar o token
+    decoded = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })[0]
+    HashWithIndifferentAccess.new(decoded)
+  rescue
+    nil
+  end
+end
